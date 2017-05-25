8f69f18bba706dc186bc722a05606d9d3585a80f
kamontat <kamontat_c@hotmail.com>
2017-05-25 23:00:29 +0700

    implement population (2)

--- app/src/main/java/com/softspec/finalproj/gameofearth/model/game/GameLogic.java
+++ app/src/main/java/com/softspec/finalproj/gameofearth/model/game/GameLogic.java
@@ -8,6 +8,7 @@ import com.softspec.finalproj.gameofearth.api.management.ImageManagement;
 import com.softspec.finalproj.gameofearth.model.strategy.CO2Strategy;
 import com.softspec.finalproj.gameofearth.model.strategy.CityStrategy;
 import com.softspec.finalproj.gameofearth.model.strategy.GameStrategy;
+import com.softspec.finalproj.gameofearth.model.strategy.PopulationStrategy;
 
 import java.io.Serializable;
 import java.util.*;
@@ -34,6 +35,7 @@ public class GameLogic extends Observable implements Serializable {
 	private GameStrategy gameStrategy;
 	private CityStrategy cityStrategy;
 	private CO2Strategy co2Strategy;
+	private PopulationStrategy populationStrategy;
 	
 	/**
 	 * current population
@@ -55,10 +57,11 @@ public class GameLogic extends Observable implements Serializable {
 	
 	private ImageManagement imageManagement;
 	
-	public GameLogic(Context c, GameStrategy gameStrategy, CityStrategy cityStrategy, CO2Strategy co2Strategy) {
+	public GameLogic(Context c, GameStrategy gameStrategy, CityStrategy cityStrategy, CO2Strategy co2Strategy, PopulationStrategy populationStrategy) {
 		this.gameStrategy = gameStrategy;
 		this.cityStrategy = cityStrategy;
 		this.co2Strategy = co2Strategy;
+		this.populationStrategy = populationStrategy;
 		
 		currentPopulation = 100;
 		co2 = gameStrategy.getDefaultCO2();
@@ -84,6 +87,10 @@ public class GameLogic extends Observable implements Serializable {
 		return co2Strategy;
 	}
 	
+	public PopulationStrategy getPopulationStrategy() {
+		return populationStrategy;
+	}
+	
 	public long getCo2() {
 		return co2;
 	}
@@ -92,8 +99,12 @@ public class GameLogic extends Observable implements Serializable {
 		return population;
 	}
 	
-	public void updatePopulation(Percent percent) {
+	public void addPopulation(Percent percent) {
 		population.add(percent);
+	}
+	
+	public void removePopulation(Percent percent) {
+		population.remove(percent);
 	}
 	
 	public void updateCO2(long add) {
@@ -118,8 +129,8 @@ public class GameLogic extends Observable implements Serializable {
 	}
 	
 	public void startGame() {
-		runService.scheduleWithFixedDelay(getUpdatePopTask(), 0, UPDATE_POPULATION_SECOND, TimeUnit.SECONDS);
-		runService.scheduleWithFixedDelay(getUpdateDateTask(), 0, UPDATE_DATE_SECOND, TimeUnit.SECONDS);
+		runService.scheduleWithFixedDelay(getUpdatePopTask(), UPDATE_POPULATION_SECOND, UPDATE_POPULATION_SECOND, TimeUnit.SECONDS);
+		runService.scheduleWithFixedDelay(getUpdateDateTask(), UPDATE_DATE_SECOND, UPDATE_DATE_SECOND, TimeUnit.SECONDS);
 	}
 	
 	public void stopGame() {
@@ -135,18 +146,21 @@ public class GameLogic extends Observable implements Serializable {
 			@Override
 			public void run() {
 				long add = (long) Math.ceil(currentPopulation * population.percent()); // increase
-				long decrease1 = (long) Math.ceil(currentPopulation * co2Strategy.calculationFromCO2(co2).percent()); // decrease;
+				long decrease1 = (long) Math.ceil(currentPopulation * populationStrategy.calculationFromCO2(co2).percent()); // decrease;
 				// update pop
 				currentPopulation += add;
 				currentPopulation -= decrease1;
-				// update co2
+				
+				// update co2, population
 				updateCO2((long) Math.ceil(co2Strategy.calculationFromPopulation(currentPopulation).percent()));
+				removePopulation(populationStrategy.calculationToThis(co2));
+				
 				// notify observer
 				setChanged();
 				notifyObservers();
 				// log
-				Log.i("UPDATE POPULATION TO", "ADD: " + add + ", SUB1: " + decrease1 + ", RESULT: " + String.valueOf(currentPopulation));
-				Log.i("UPDATE CO2 TO", String.valueOf(co2));
+				Log.i("UPDATE", "population: " + population + ", co2: " + co2);
+				Log.i("CURRENT POPULATION", "ADD: " + add + ", SUB1: " + decrease1 + ", RESULT: " + String.valueOf(currentPopulation));
 			}
 		};
 	}
@@ -158,7 +172,7 @@ public class GameLogic extends Observable implements Serializable {
 				date.set(Calendar.DAY_OF_MONTH, date.get(Calendar.DAY_OF_MONTH) + 1);
 				setChanged();
 				notifyObservers();
-				Log.i("UPDATE DATE TO", date.toString());
+				Log.i("UPDATE DATE TO", String.valueOf(getDate()));
 			}
 		};
 	}
