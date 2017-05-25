6c8f7ad0674733f9e6bfc952f9e96d7b620e3723
Soraya Saenna <bubblebitoey@gmail.com>
2017-05-26 02:12:14 +0700

    pull recently work

--- app/src/main/java/com/softspec/finalproj/gameofearth/api/management/DatabaseManagement.java
+++ app/src/main/java/com/softspec/finalproj/gameofearth/api/management/DatabaseManagement.java
@@ -18,9 +18,6 @@ import java.util.*;
 public class DatabaseManagement extends Observable implements Runnable {
 	private Database database;
 	
-	public DatabaseManagement() {
-	
-	}
 	public DatabaseManagement(Context c) {
 		database = new Database(c);
 	}
