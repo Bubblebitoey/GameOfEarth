39054182261749b3284f6edb33e7f5c4d834188b
kamontat <kamontat_c@hotmail.com>
2017-05-26 19:09:49 +0700

    Fixed End activity

--- app/src/main/java/com/softspec/finalproj/gameofearth/view/EndActivity.java
+++ app/src/main/java/com/softspec/finalproj/gameofearth/view/EndActivity.java
@@ -1,38 +1,29 @@
 package com.softspec.finalproj.gameofearth.view;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import com.softspec.finalproj.gameofearth.R;
-import com.softspec.finalproj.gameofearth.model.game.GameLogic;
 
 /**
  * Created by bubblebitoey on 5/26/2017 AD.
  */
 public class EndActivity extends FullScreenActivity {
-	private GameLogic gameLogic;
-	private ImageButton playAgainBtn;
-	private TextView scoreTxt;
-	
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_end);
 		
-		gameLogic = (GameLogic) getIntent().getSerializableExtra(LoadedProgressActivity.GAME_LOGIC);
+		// ImageButton playAgainBtn = (ImageButton) findViewById(R.id.playAgain_button);
+		TextView scoreTxt = (TextView) findViewById(R.id.txtScore);
+		scoreTxt.setText(String.valueOf(MainActivity.logic.getHighestPopulation()));
 		
-		playAgainBtn = (ImageButton) findViewById(R.id.playAgain_button);
-		scoreTxt = (TextView) findViewById(R.id.txtScore);
-		
-		playAgainBtn.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(EndActivity.this, StartActivity.class);
-				startActivity(intent);
-			}
-		});
+		// playAgainBtn.setOnClickListener(new View.OnClickListener() {
+		//	@Override
+		//	public void onClick(View v) {
+		//		Intent intent = new Intent(EndActivity.this, StartActivity.class);
+		//		startActivity(intent);
+		// 	}
+		// });
 	}
 }
