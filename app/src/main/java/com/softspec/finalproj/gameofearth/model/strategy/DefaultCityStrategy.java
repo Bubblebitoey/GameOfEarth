3cfc34177a07d268d2f91831fb1303e55392fea3
kamontat <kamontat_c@hotmail.com>
2017-05-26 16:39:03 +0700

    stable city

--- app/src/main/java/com/softspec/finalproj/gameofearth/model/strategy/DefaultCityStrategy.java
+++ app/src/main/java/com/softspec/finalproj/gameofearth/model/strategy/DefaultCityStrategy.java
@@ -17,13 +17,13 @@ public class DefaultCityStrategy implements CityStrategy {
 		long population = logic.getCurrentPopulation();
 		if (population > 1_000_000L) {
 			return CityLevel.LEVEL_5;
-		} else if (population > 10_000L) {
+		} else if (population > 100_000L) {
 			return CityLevel.LEVEL_4;
-		} else if (population > 1_000L) {
+		} else if (population > 75_000L) {
 			return CityLevel.LEVEL_3;
-		} else if (population > 500L) {
+		} else if (population > 10_000L) {
 			return CityLevel.LEVEL_2;
-		} else if (population > 150L) {
+		} else if (population > 1000L) {
 			return CityLevel.LEVEL_1;
 		} else {
 			return CityLevel.LEVEL_0;
